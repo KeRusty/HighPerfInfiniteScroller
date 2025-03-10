ab700b406916477f422d6f2a79fbbb5636e29931
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-03-10 19:26:55 +0400

    created routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// Components
+import {AppHeader} from '../components';
+
+// Routes
+import HomeScreen from '../screens/home/Home';
+
+const Stack = createNativeStackNavigator();
+function MainStack() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          header: () => {
+            return <AppHeader title={'Infinte Post List'} />;
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default MainStack;
