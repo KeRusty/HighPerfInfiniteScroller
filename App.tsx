d397d37ccd9fb4cdfd16f6f655eeb48af7f1bb48
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-03-10 17:43:17 +0400

    Added requried packages

--- App.tsx
+++ App.tsx
@@ -1,41 +1,8 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+import {SafeAreaView} from 'react-native';
 
 function App(): React.JSX.Element {
-
-  return (
-    <SafeAreaView >
-
-    </SafeAreaView>
-  );
+  return <SafeAreaView />;
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
